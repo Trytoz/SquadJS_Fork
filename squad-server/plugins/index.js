9fc683824e4a8bf50c02621aa3acdde8ef3d5b44
Trytoz <410855403@qq.com>
2024-08-16 15:03:15 +0800

    Update index.js
    汉化部分提示

--- squad-server/plugins/index.js
+++ squad-server/plugins/index.js
@@ -2,49 +2,49 @@ import fs from 'fs';
 
 import Logger from 'core/logger';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 class Plugins {
-  constructor() {
-    this.plugins = null;
-  }
-
-  async getPlugins(force = false) {
-    if (this.plugins && !force) return this.plugins;
-
-    this.plugins = {};
-
-    const dir = await fs.promises.opendir(path.join(__dirname, './'));
-
-    const pluginFilenames = [];
-    for await (const dirent of dir) {
-      // Check for non .js file type
-      if (!dirent.isFile() || !dirent.name.endsWith('.js')) {
-        continue;
-      }
-      if (
-        [
-          'index.js',
-          'base-plugin.js',
-          'discord-base-message-updater.js',
-          'discord-base-plugin.js',
-          'readme.md'
-        ].includes(dirent.name)
-      )
-        continue;
-      pluginFilenames.push(dirent.name);
+    constructor() {
+        this.plugins = null;
     }
 
-    for (const pluginFilename of pluginFilenames) {
-      Logger.verbose('Plugins', 1, `Loading plugin file ${pluginFilename}...`);
-      const { default: Plugin } = await import(`./${pluginFilename}`);
-      this.plugins[Plugin.name] = Plugin;
-    }
+    async getPlugins(force = false) {
+        if (this.plugins && !force) return this.plugins;
 
-    return this.plugins;
-  }
+        this.plugins = {};
+
+        const dir = await fs.promises.opendir(path.join(__dirname, './'));
+
+        const pluginFilenames = [];
+        for await (const dirent of dir) {
+            // Check for non .js file type
+            if (!dirent.isFile() || !dirent.name.endsWith('.js')) {
+                continue;
+            }
+            if (
+                [
+                    'index.js',
+                    'base-plugin.js',
+                    'discord-base-message-updater.js',
+                    'discord-base-plugin.js',
+                    'readme.md'
+                ].includes(dirent.name)
+            )
+                continue;
+            pluginFilenames.push(dirent.name);
+        }
+
+        for (const pluginFilename of pluginFilenames) {
+            Logger.verbose('Plugins', 1, `正在加载插件文件 ${pluginFilename}...`);
+            const {default: Plugin} = await import(`./${pluginFilename}`);
+            this.plugins[Plugin.name] = Plugin;
+        }
+
+        return this.plugins;
+    }
 }
 
 export default new Plugins();
