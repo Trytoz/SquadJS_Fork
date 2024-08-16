56c56f42d4fe42af94d69830e15bc6a782670ce9
Trytoz <410855403@qq.com>
2024-08-16 14:11:13 +0800

    Update layers.js
    汉化提示

--- squad-server/layers/layers.js
+++ squad-server/layers/layers.js
@@ -5,54 +5,54 @@ import Logger from 'core/logger';
 import Layer from './layer.js';
 
 class Layers {
-  constructor() {
-    this.layers = [];
+    constructor() {
+        this.layers = [];
 
-    this.pulled = false;
-  }
-
-  async pull(force = false) {
-    if (this.pulled && !force) {
-      Logger.verbose('Layers', 2, 'Already pulled layers.');
-      return;
-    }
-    if (force) Logger.verbose('Layers', 1, 'Forcing update to layer information...');
-
-    this.layers = [];
-
-    Logger.verbose('Layers', 1, 'Pulling layers...');
-	console.log('Layers.url:','https://raw.gitmirror.comSquad-Wiki/squad-wiki-pipeline-map-data/master/completed_output/_Current%20Version/finished.json')
-    const response = await axios.get(
-      'https://raw.gitmirror.comSquad-Wiki/squad-wiki-pipeline-map-data/master/completed_output/_Current%20Version/finished.json'
-    );
-
-    for (const layer of response.data.Maps) {
-      this.layers.push(new Layer(layer));
+        this.pulled = false;
     }
 
-    Logger.verbose('Layers', 1, `Pulled ${this.layers.length} layers.`);
+    async pull(force = false) {
+        if (this.pulled && !force) {
+            Logger.verbose('Layers', 2, '已经拉取过地图图层信息.');
+            return;
+        }
+        if (force) Logger.verbose('Layers', 1, '强制更新地图图层信息...');
 
-    this.pulled = true;
+        this.layers = [];
 
-    return this.layers;
-  }
+        Logger.verbose('Layers', 1, '正在拉取地图图层信息...');
+        Logger.verbose('Layers.url:', 1, 'https://raw.gitmirror.comSquad-Wiki/squad-wiki-pipeline-map-data/master/completed_output/_Current%20Version/finished.json')
+        const response = await axios.get(
+            'https://raw.gitmirror.comSquad-Wiki/squad-wiki-pipeline-map-data/master/completed_output/_Current%20Version/finished.json'
+        );
 
-  async getLayerByCondition(condition) {
-    await this.pull();
+        for (const layer of response.data.Maps) {
+            this.layers.push(new Layer(layer));
+        }
 
-    const matches = this.layers.filter(condition);
-    if (matches.length === 1) return matches[0];
+        Logger.verbose('Layers', 1, `Pulled ${this.layers.length} layers.`);
 
-    return null;
-  }
+        this.pulled = true;
 
-  getLayerById(layerId) {
-    return this.getLayerByCondition((layer) => layer.layerid === layerId);
-  }
+        return this.layers;
+    }
 
-  getLayerByClassname(classname) {
-    return this.getLayerByCondition((layer) => layer.classname === classname);
-  }
+    async getLayerByCondition(condition) {
+        await this.pull();
+
+        const matches = this.layers.filter(condition);
+        if (matches.length === 1) return matches[0];
+
+        return null;
+    }
+
+    getLayerById(layerId) {
+        return this.getLayerByCondition((layer) => layer.layerid === layerId);
+    }
+
+    getLayerByClassname(classname) {
+        return this.getLayerByCondition((layer) => layer.classname === classname);
+    }
 }
 
 export default new Layers();
